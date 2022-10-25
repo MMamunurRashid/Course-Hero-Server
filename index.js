6542f790d182ca8fe4e5ee98b39d409d5893e522
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-10-25 17:44:14 +0600

    each course added and loaded in server

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 
 const courses = require("./data/courses.json");
-
+const course = require("./data/course.json");
 app.get("/", (req, res) => {
   res.send("hello server");
 });
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const requestedCourse = course.find((c) => c.id == id);
+  res.send(requestedCourse);
 });
 
 app.listen(Port, () => {
